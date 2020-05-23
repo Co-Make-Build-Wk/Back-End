7d56bfe15eb35c46e130a5b74ed61766fc98a2cc
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-05-22 21:35:25 -0400

    setting up production object

--- knexfile.js
+++ knexfile.js
@@ -39,19 +39,18 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'sqlite3',
+    connection: process.env.DATABASE_URL,
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      }
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+      tableName: 'db_migrations',
+    },
   }
 
 };
